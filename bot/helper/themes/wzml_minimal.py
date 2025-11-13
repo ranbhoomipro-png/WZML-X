c7307b566f456a4d9e20c7dcba15293e256f5876
ranbhoomipro-png <ranbhoomipro@gmail.com>
2025-11-13 13:29:33 +0530

    Update wzml_minimal.py
    Signed-off-by: ranbhoomipro-png <ranbhoomipro@gmail.com>

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -2,14 +2,14 @@
 class WZMLStyle:
     # ----------------------
     # async def start(client, message) ---> __main__.py
-    ST_BN1_NAME = "Repo"
-    ST_BN1_URL = "https://www.github.com/weebzone/WZML-X"
-    ST_BN2_NAME = "Updates"
-    ST_BN2_URL = "https://t.me/WZML_X"
-    ST_MSG = """<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
+    ST_BN1_NAME = "✨Join CHannel✨"
+    ST_BN1_URL = "https://t.me/+eldH4g3FY6JlMDY9"
+    ST_BN2_NAME = "⚡DL-Group⚡"
+    ST_BN2_URL = "https://t.me/+e9KSGZFoxv03NjQ1"
+    ST_MSG = """<i>This bot allows you to download non-DRM educational videos from various platforms and mirror them to your preferred cloud storage like Google Drive, any rclone cloud, or even send them to Telegram or DDL servers.</i>
 <b>Type {help_command} to get a list of available commands</b>"""
     ST_BOTPM = """<i>Now, This bot will send all your files and links here. Start Using ...</i>"""
-    ST_UNAUTH = """<i>You Are not authorized user! Deploy your own WZML-X Mirror-Leech bot</i>"""
+    ST_UNAUTH = """<i>You Are not authorized user!</i>"""
     OWN_TOKEN_GENERATE = (
         """<b>Temporary Token is not yours!</b>\n\n<i>Kindly generate your own.</i>"""
     )
